e96cd396a79be227938bc33ced417b973d2e21ea
kim-jihyeeee <lovejh4840@gmail.com>
2026-03-26 21:43:42 +0900

    Delete app/api/monitor-batch directory

--- app/api/monitor-batch/route.js
+++
@@ -1,9 +0,0 @@
-import { scrapeNaverBatchRanks } from "../../../lib/naverSearchScraper";
-
-export async function POST(req) {
-  const { keywords, domains } = await req.json();
-
-  const rows = await scrapeNaverBatchRanks(keywords, domains);
-
-  return Response.json({ rows });
-}
